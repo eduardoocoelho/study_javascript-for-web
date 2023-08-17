428beca15cdf0684c40f66d3316853704805751f
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-08-17 14:28:26 -0300

    reducing and reusing code with foreach

--- 2. ws - JavaScript_Manipulando DOM/robotron2000/js/main.js
+++ 2. ws - JavaScript_Manipulando DOM/robotron2000/js/main.js
@@ -2,10 +2,19 @@ const plus = document.querySelector('#somar');
 const minus = document.querySelector('#subtrair');
 const arm = document.querySelector('#braco');
 
-plus.addEventListener("click", (event) => {
-    arm.value = Number(arm.value) + 1;
-})
+const control = document.querySelectorAll(".controle-ajuste");
 
-minus.addEventListener("click", (event) => {
-    arm.value = Number(arm.value) - 1;
-})
+control.forEach( (element) => {
+    element.addEventListener("click", (event) => {
+        manipulateData(event.target.textContent);
+    });
+});
+
+function manipulateData (op) {
+    if(op === "+") {
+        arm.value = Number(arm.value) + 1;
+    }
+    else{
+        arm.value = Number(arm.value) - 1;
+    }
+}
