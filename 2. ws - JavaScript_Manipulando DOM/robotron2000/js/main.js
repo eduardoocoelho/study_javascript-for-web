7fb7fe3cbb4aebdccbe1376cd7fcb062962fc466
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-08-17 15:07:11 -0300

    using data-attributes

--- 2. ws - JavaScript_Manipulando DOM/robotron2000/js/main.js
+++ 2. ws - JavaScript_Manipulando DOM/robotron2000/js/main.js
@@ -1,13 +1,13 @@
-const control = document.querySelectorAll(".controle-ajuste");
+const control = document.querySelectorAll("[data-control]");
 
 control.forEach( (element) => {
     element.addEventListener("click", (event) => {
-        manipulateData(event.target.textContent, event.target.parentNode);
+        manipulateData(event.target.dataset.control, event.target.parentNode);
     });
 });
 
 function manipulateData (op, parent) {
-    const part = parent.querySelector('.controle-contador');
+    const part = parent.querySelector('[data-counter]');
 
     if(op === "+") {
         part.value = Number(part.value) + 1;
