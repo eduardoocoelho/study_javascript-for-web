512624e5b59d5cf0ab1bb7216dbaca695fa8fd3e
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-08-17 10:05:45 -0300

    .onkeydown and .onkeyup + fixing possible null parameters

--- ws - JavaScript para Web/aluramidi/main.js
+++ ws - JavaScript para Web/aluramidi/main.js
@@ -1,15 +1,38 @@
 function playSound (buttonId) {
-    document.querySelector(buttonId).play();
+    const audio = document.querySelector(buttonId);
+
+    if(audio != null && audio.localName === 'audio'){
+        audio.play();
+    }
+    else{
+        console.log('Element not found.')
+    }
 }
 
 const buttonList = document.querySelectorAll('.tecla');
 
 for (let counter = 0; counter < buttonList.length; counter++) {
+
+    //reuse the same code
+    const key = buttonList[counter];
+
     //template string
-    const sound = `#som_${buttonList[counter].classList[1]}`;
+    const sound = `#som_${key.classList[1]}`;
 
     //anonymous function
-    buttonList[counter].onclick = function () {
+    key.onclick = function () {
         playSound(sound);
+    }
+
+    //press key
+    key.onkeydown = function (event) {
+        if(event.code === "Enter" || event.code === "Space"){
+            key.classList.add('ativa');
+        }
+    }
+
+    //release key
+    key.onkeyup = function () {
+        key.classList.remove('ativa');
     }
 }
