5c732428b3516b293e7dfd5d46bc6d1169efc493
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-08-19 19:41:18 -0300

    updating an existing item

--- 3. ws - JavaScript na Web_dados no navegador/mochila-de-viagem/js/main.js
+++ 3. ws - JavaScript na Web_dados no navegador/mochila-de-viagem/js/main.js
@@ -9,7 +9,7 @@ const items = JSON.parse(localStorage.getItem("items")) || [];
 
 //Percorrendo os itens salvos no localStorage ao carregar a página e adicionando os itens na lista
 items.forEach( (element) => {
-    buildElement(element);
+    buildItem(element);
 });
 
 
@@ -20,19 +20,30 @@ form.addEventListener("submit", (event) => {
     const name = event.target.elements['nome'];
     const quantity =  event.target.elements['quantidade'];
     
+    //Procura um elemento já existente na lista pelo nome
+    const exist = items.find(element => element.name === name.value);
+
     //Criando um objeto do novo item com os dados passados pelo submit
     const currentItem = {
         "name": name.value,
         "quantity": quantity.value
     };
 
-    //Inserindo o objeto ao final do array (.push)
-    items.push(currentItem);
+    //Se o item existir, atualiza o ID de identificação e atualiza o item
+    if(exist){
+        currentItem.id = exist.id;
+        updateItem(currentItem);
+    } else{ //Se não existir, cria um novo item 
+        currentItem.id = items.length;
+
+        buildItem(currentItem);
+
+        //Inserindo o objeto ao final do array (.push)
+        items.push(currentItem);
+    }
 
     //Armazenando os dados do novo item no localStorage convertendo os objetos do array para string
     localStorage.setItem("items", JSON.stringify(items));
-
-    buildElement(currentItem);
 
     //Zerando os campos do formulário após o envio
     name.value = "";
@@ -41,7 +52,7 @@ form.addEventListener("submit", (event) => {
 
 
 //Criar um novo elemento na lista
-function buildElement(item) {
+function buildItem(item) {
     //Cria o elemento 'li' e adiciona a classe 'item' à esse elemento
     const newItem = document.createElement('li');
     newItem.classList.add("item");
@@ -49,11 +60,16 @@ function buildElement(item) {
     //Cria o elemento 'strong' que vai dentro da 'li' criada e atribui a quantidade passada por parâmetro
     const numberItem = document.createElement('strong');
     numberItem.innerHTML = item.quantity;
+    numberItem.dataset.id = item.id; //Atualiza o ID do valor do item
 
-    //Adiciona o elemento 'strong' criado como filho da 'li' e adiciona o nome passado como parâmetro à 'li'
+    //Adiciona o elemento 'strong' criado como filho da 'li' e atribui o nome passado como parâmetro à 'li' como valor
     newItem.appendChild(numberItem);
     newItem.innerHTML += item.name;
 
     //Adiciona a 'li' criada a lista já existente
     list.appendChild(newItem);
+}
+
+function updateItem(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantity;
 }
