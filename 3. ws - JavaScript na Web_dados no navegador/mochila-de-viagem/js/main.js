42ae22a126b5ea919f28314a0bc3a052a15764fd
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-08-19 19:17:52 -0300

    refactoring the code + building inital list with localStorage data

--- 3. ws - JavaScript na Web_dados no navegador/mochila-de-viagem/js/main.js
+++ 3. ws - JavaScript na Web_dados no navegador/mochila-de-viagem/js/main.js
@@ -6,41 +6,24 @@ const list = document.getElementById("list");
 //JSON.parse para a string (JSON.stringify()) em objetos
 const items = JSON.parse(localStorage.getItem("items")) || [];
 
+
+//Percorrendo os itens salvos no localStorage ao carregar a página e adicionando os itens na lista
+items.forEach( (element) => {
+    buildElement(element);
+});
+
+
 //Localiza o evento de submit do formulário
 form.addEventListener("submit", (event) => {
     event.preventDefault(); //Previne o funcionamento padrão do formulário
     
     const name = event.target.elements['nome'];
-    const quant =  event.target.elements['quantidade'];
+    const quantity =  event.target.elements['quantidade'];
     
-    buildElement(name.value, quant.value);
-
-    //Zerando os campos do formulário após o envio
-    name.value = "";
-    quant.value = "";
-});
-
-//Criar um novo elemento na lista
-function buildElement(name, quantity) {
-    //Cria o elemento 'li' e adiciona a classe 'item' à esse elemento
-    const newItem = document.createElement('li');
-    newItem.classList.add("item");
-
-    //Cria o elemento 'strong' que vai dentro da 'li' criada e atribui a quantidade passada por parâmetro
-    const numberItem = document.createElement('strong');
-    numberItem.innerHTML = quantity;
-
-    //Adiciona o elemento 'strong' criado como filho da 'li' e adiciona o nome passado como parâmetro à 'li'
-    newItem.appendChild(numberItem);
-    newItem.innerHTML += name;
-
-    //Adiciona a 'li' criada a lista já existente
-    list.appendChild(newItem);
-
     //Criando um objeto do novo item com os dados passados pelo submit
     const currentItem = {
-        "name": name,
-        "quantity": quantity
+        "name": name.value,
+        "quantity": quantity.value
     };
 
     //Inserindo o objeto ao final do array (.push)
@@ -48,4 +31,29 @@ function buildElement(name, quantity) {
 
     //Armazenando os dados do novo item no localStorage convertendo os objetos do array para string
     localStorage.setItem("items", JSON.stringify(items));
+
+    buildElement(currentItem);
+
+    //Zerando os campos do formulário após o envio
+    name.value = "";
+    quantity.value = "";
+});
+
+
+//Criar um novo elemento na lista
+function buildElement(item) {
+    //Cria o elemento 'li' e adiciona a classe 'item' à esse elemento
+    const newItem = document.createElement('li');
+    newItem.classList.add("item");
+
+    //Cria o elemento 'strong' que vai dentro da 'li' criada e atribui a quantidade passada por parâmetro
+    const numberItem = document.createElement('strong');
+    numberItem.innerHTML = item.quantity;
+
+    //Adiciona o elemento 'strong' criado como filho da 'li' e adiciona o nome passado como parâmetro à 'li'
+    newItem.appendChild(numberItem);
+    newItem.innerHTML += item.name;
+
+    //Adiciona a 'li' criada a lista já existente
+    list.appendChild(newItem);
 }
