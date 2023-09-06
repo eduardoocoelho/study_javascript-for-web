0dbb8c110942049aed4e35a130ef581015f13631
Eduardo Oliveira Coelho <1334354@sga.pucminas.br>
2023-09-06 11:26:10 -0300

    changing the button focus and music button working

--- 2.1 ws  - JavaScript_Manipulando DOM/fokus/main.js
+++ 2.1 ws  - JavaScript_Manipulando DOM/fokus/main.js
@@ -4,22 +4,44 @@ const shortBt = document.querySelector('.app__card-button--curto');
 const longBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
+const buttons = document.querySelectorAll('.app__card-button');
+const musicInput = document.querySelector('#alternar-musica');
+const music = new Audio('sons/luna-rise-part-one.mp3')
+music.loop = true;
+
+musicInput.addEventListener('change', () => {
+    music.currentTime = 0;
+    if(music.paused){
+        music.play();
+    }
+    else{
+        music.pause();
+    }
+});
 
 focusBt.addEventListener('click', () => {
     changeAttribute('foco');
+    focusBt.classList.add('active');
 });
 
 shortBt.addEventListener('click', () => {
     changeAttribute('descanso-curto');
+    shortBt.classList.add('active');
 });
 
 longBt.addEventListener('click', () => {
     changeAttribute('descanso-longo');
+    longBt.classList.add('active');
 });
 
 function changeAttribute(value) {
+    buttons.forEach(function (value){
+        value.classList.remove('active');
+    });
+
     html.setAttribute('data-contexto', value);
     banner.setAttribute('src', `imagens/${value}.png`);
+
     switch (value) {
         case 'foco':
             title.innerHTML = `
