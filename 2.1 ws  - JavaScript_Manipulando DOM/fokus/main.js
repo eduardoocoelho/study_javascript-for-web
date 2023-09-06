0fb19f0fb94bdc628c0071ff7ee070d3710fbaf3
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-09-05 21:03:32 -0300

    refactoring the code and changing the title

--- 2.1 ws  - JavaScript_Manipulando DOM/fokus/main.js
+++ 2.1 ws  - JavaScript_Manipulando DOM/fokus/main.js
@@ -1,16 +1,41 @@
 const html = document.querySelector('html');
-const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo');
+const focusBt = document.querySelector('.app__card-button--foco');
+const shortBt = document.querySelector('.app__card-button--curto');
+const longBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const title = document.querySelector('.app__title');
 
-focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
+focusBt.addEventListener('click', () => {
+    changeAttribute('foco');
 });
 
-curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
+shortBt.addEventListener('click', () => {
+    changeAttribute('descanso-curto');
 });
 
-longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
+longBt.addEventListener('click', () => {
+    changeAttribute('descanso-longo');
 });
+
+function changeAttribute(value) {
+    html.setAttribute('data-contexto', value);
+    banner.setAttribute('src', `imagens/${value}.png`);
+    switch (value) {
+        case 'foco':
+            title.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+
+            break;
+        case 'descanso-curto':
+            title.innerHTML = `
+            Que tal dar uma respirada?<br> 
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            
+            break;
+        case 'descanso-longo':
+            title.innerHTML = `
+            Hora de voltar à superfície.<br> 
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+    }
+}
