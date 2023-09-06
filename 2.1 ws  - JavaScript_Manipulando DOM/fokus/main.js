839a9ea279131c1668a0125b4316ebbc5a6c5ec6
Eduardo Oliveira Coelho <1334354@sga.pucminas.br>
2023-09-06 13:52:15 -0300

    timer running on console

--- 2.1 ws  - JavaScript_Manipulando DOM/fokus/main.js
+++ 2.1 ws  - JavaScript_Manipulando DOM/fokus/main.js
@@ -6,7 +6,15 @@ const banner = document.querySelector('.app__image');
 const title = document.querySelector('.app__title');
 const buttons = document.querySelectorAll('.app__card-button');
 const musicInput = document.querySelector('#alternar-musica');
-const music = new Audio('sons/luna-rise-part-one.mp3')
+const startPauseBt = document.querySelector('#start-pause')
+const music = new Audio('sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('sons/play.wav');
+const audioPause = new Audio('sons/pause.mp3');
+const audioFinished = new Audio('sons/beep.mp3');
+
+let runtimeSec = 5
+let intervalId = null
+
 music.loop = true;
 
 musicInput.addEventListener('change', () => {
@@ -60,4 +68,32 @@ function changeAttribute(value) {
             Hora de voltar à superfície.<br> 
             <strong class="app__title-strong">Faça uma pausa longa.</strong>`
     }
+}
+
+const countdown = () => {
+    if(runtimeSec <= 0){
+        audioFinished.play();
+        alert('Tempo finalizado!');
+        reset();
+        return;
+    }
+    runtimeSec -= 1;
+    console.log('Temporizador: ' + runtimeSec);
+}
+
+startPauseBt.addEventListener('click', playOrPause);
+
+function playOrPause() {
+    if(intervalId){
+        audioPause.play();
+        reset();
+        return;
+    }
+    audioPlay.play();
+    intervalId = setInterval(countdown, 1000);
+}
+
+function reset() {
+    clearInterval(intervalId);
+    intervalId = null;
 }
