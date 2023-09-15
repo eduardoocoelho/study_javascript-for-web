22f114aafd17250fd5121b2578945fab82a6ef49
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-09-15 18:29:07 -0300

    showing the CEP search results on page + course finished

--- 5.ws_js-consuming-data-API/alura-books-form/js/main.js
+++ 5.ws_js-consuming-data-API/alura-books-form/js/main.js
@@ -6,9 +6,21 @@ async function searchCEP(cep){
         if(jsonCEP.erro){
             throw Error('CEP inválido!');
         }
+
+        var city = document.getElementById('cidade');
+        var street = document.getElementById('endereco');
+        var state = document.getElementById('estado');
+        var neighborhood = document.getElementById('bairro');
+
+        city.value = jsonCEP.localidade;
+        street.value = jsonCEP.logradouro;
+        state.value = jsonCEP.uf;
+        neighborhood.value = jsonCEP.bairro;
+
         console.log(jsonCEP);
         return jsonCEP;
     } catch (e){
+        alert('CEP inválido! Tente novamente.')
         console.log(e);
     }
 }
