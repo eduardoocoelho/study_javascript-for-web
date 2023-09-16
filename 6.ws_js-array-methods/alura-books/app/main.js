1b337fd7e80481fab070061ce176d45f35e60c5c
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-09-16 10:51:40 -0300

    showing each book on page using forEach

--- 6.ws_js-array-methods/alura-books/app/main.js
+++ 6.ws_js-array-methods/alura-books/app/main.js
@@ -1,11 +1,31 @@
 let books = [];
-let endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+const booksElement = document.getElementById('livros');
 
 getBooksFromAPI();
 
 async function getBooksFromAPI(){
-    let answer = await fetch(endpointAPI);
+    const answer = await fetch(endpointAPI);
     books = await answer.json();
 
     console.table(books);
+
+    showBooks(books);
+}
+
+function showBooks(booksList){
+    booksList.forEach(book => {
+        booksElement.innerHTML += `
+        <div class="livro">
+        <img class="livro__imagens" src="${book.imagem}" 
+        alt="${book.alt}" />
+        <h2 class="livro__titulo">${book.titulo}</h2>
+        <p class="livro__descricao">${book.autor}</p>
+        <p class="livro__preco" id="preco">R$${book.preco}</p>
+        <div class="tags">
+            <span class="tag">${book.categoria}</span>
+        </div>
+        </div>
+        `
+    });
 }
